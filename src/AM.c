69c029778faed81e3277fccacff755aa7e0413e8
FotiadisM <fotiadis.michalis20@gmail.com>
2019-12-10 15:33:59 +0200

    AM_CreateIndex

--- src/AM.c
+++ src/AM.c
@@ -1,3 +1,6 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "bf.h"
 #include "AM.h"
 
 int AM_errno = AME_OK;
@@ -6,12 +9,43 @@ void AM_Init() {
 	return;
 }
 
+int AM_CreateIndex(char *fileName, char attrType1, int attrLength1, char attrType2, int attrLength2) {
 
-int AM_CreateIndex(char *fileName, 
-	               char attrType1, 
-	               int attrLength1, 
-	               char attrType2, 
-	               int attrLength2) {
+  int fileDesc, joker = 1;
+  char *data;
+  BF_Block *mBlock;
+  BF_Block_Init(&mBlock);
+
+  CALL_BF(BF_CreateFile(fileName));
+  CALL_BF(BF_OpenFile(fileName, &fileDesc));
+
+  CALL_BF(BF_AllocateBlock(fileName, mBlock));
+  data = BF_Block_GetData(mBlock);
+  memset(data, 0, BF_BLOCK_SIZE); // optional
+  memcpy(data, "B+", 2*sizeof(char));
+  memcpy(data + 2*sizeof(char), &joker, sizeof(int));
+  memcpy(data + 2*sizeof(char) + sizeof(int), &attrType1, sizeof(char));
+  memcpy(data + 3*sizeof(char) + sizeof(int), &attrLength1, sizeof(int));
+  memcpy(data + 3*sizeof(char) + 2*sizeof(int), &attrType2, sizeof(char));
+  memcpy(data + 4*sizeof(char) + 2*sizeof(int), &attrLength2, sizeof(int));
+  BF_Block_SetDirty(mBlock);
+  CALL_BF(BF_UnpinBlock(mBlock));
+
+  CALL_BF(BF_AllocateBlock(fileName, mBlock));
+  data = BF_Block_GetData(mBlock);
+  joker = 2;
+  memcpy(data, &joker, sizeof(int));
+  BF_Block_SetDirty(mBlock);
+  CALL_BF(BF_UnpinBlock(mBlock));
+
+  CALL_BF(BF_AllocateBlock(fileName, mBlock));
+  data = BF_Block_GetData(mBlock);
+  memset(data, 0, BF_BLOCK_SIZE); // optional
+  BF_Block_SetDirty(mBlock);
+  CALL_BF(BF_UnpinBlock(mBlock));
+
+  BF_Block_Destroy(&mBlock);
+
   return AME_OK;
 }
 
